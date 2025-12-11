993e4227f354efde8e9f6e888226211fd77d7604
Василий Ноженькин <vasiliy.nozhenkin@mail.ru>
2025-12-11 19:18:26 +0500

    firs commit

---
+++ contracts/Collateralized Loan.sol
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// Collateralized Loan Contract
+contract CollateralizedLoan {
+    // Loan structure
+    struct Loan {
+        address borrower;
+        address lender;
+        uint collateralAmount;
+        uint loanAmount;
+        uint interestRate;
+        uint dueDate;
+        bool isFunded;
+        bool isRepaid;
+    }
+
+    // Create a mapping to manage the loans
+    mapping(uint => Loan) public loans;
+    uint public nextLoanId = 1;
+  
+  
+    // Hint: Define events for loan requested, funded, repaid, and collateral claimed
+    event LoanRequested(uint loanId, address borrower, uint collateralAmount, uint loanAmount, uint interestRate, uint dueDate);
+    event LoanFunded(uint loanId, address lender);
+    event LoanRepaid(uint loanId);
+    event CollateralClaimed(uint loanId, address lender);
+    // Custom Modifiers
+    // Hint: Write a modifier to check if a loan exists
+    modifier loanExists(uint _loanId) {
+        require(_loanId > 0 && _loanId < nextLoanId , "loanId is not correct");
+        _;
+    }
+    // Hint: Write a modifier to ensure a loan is not already funded
+    modifier loanIsNotFunded(uint _loanId) {
+        require(!loans[_loanId].isFunded , "Loan is not funded");
+        _;
+    }
+    modifier loanFunded(uint _loanId) {
+        require(loans[_loanId].isFunded, "Loan is funded");
+        _;
+    }
+
+    modifier validLoan(uint _loanId) {
+        require(_loanId > 0 && _loanId < nextLoanId, "Loan does not exist");
+        _;
+    }
+
+     modifier onlyBorrower(uint _loanId) {
+        require(loans[_loanId].borrower == msg.sender, "Only borrower has access to this action");
+        _;
+    }
+
+    modifier onlyLender(uint _loanId) {
+        require(loans[_loanId].lender == msg.sender, "Only lender has access to this action");
+        _;
+    }
+
+    modifier notRepaid(uint _loanId) {
+        require(!loans[_loanId].isRepaid, "Loan is already repaid");
+        _;
+    }
+    // Function to deposit collateral and request a loan
+    function depositCollateralAndRequestLoan(uint _interestRate, uint _duration) external payable {
+        // Hint: Check if the collateral is more than 0
+        uint collateral = msg.value; 
+        //Some additional validation
+        require(collateral > 0, "Collateral must be greater than 0");
+        require(_interestRate > 0 && _interestRate < 99, "Interest rate must be a number less then 100");
+        require(_duration > 0, "Duration must be greater than 0");
+
+        uint loanId = nextLoanId;
+
+        uint dueDate = block.timestamp + _duration;
+        // Hint: Calculate the loan amount based on the collateralized amount
+        //Basic algo
+        uint loanAmount = (collateral * 100) / (100 + _interestRate);
+        // Hint: Increment nextLoanId and create a new loan in the loans mapping
+        nextLoanId += 1;
+        
+    
+        loans[loanId] = Loan({
+            borrower: msg.sender,
+            lender: address(0),
+            collateralAmount: msg.value,
+            loanAmount: loanAmount,
+            interestRate: _interestRate,
+            dueDate: dueDate,
+            isFunded: false,
+            isRepaid: false
+        });
+
+        // Hint: Emit an event for loan request
+        emit LoanRequested(loanId, msg.sender, collateral, loanAmount, _interestRate, dueDate);
+    }
+
+  
+    // Function to fund a loan
+    // Hint: Write the fundLoan function with necessary checks and logic
+    function fundLoad(uint _loanId) public payable {
+        require(_loanId > 0 && _loanId < nextLoanId, "Loan does not exist");
+        Loan storage loan = loans[_loanId];
+        require(msg.value >= loan.loanAmount, "Insufficient amount of funds for loan");
+        loan.lender = msg.sender;
+        loan.isFunded = true;
+        //send & transfer available for payable onnly
+        payable(loan.borrower).transfer(loan.loanAmount);
+
+        emit LoanFunded(_loanId, msg.sender);
+    }
+
+    // Function to repay a loan
+    // Hint: Write the repayLoan function with necessary checks and logic
+    function repayLoan(uint _loanId) public payable {
+        require(_loanId > 0 && _loanId < nextLoanId, "Loan does not exist");
+        
+        Loan storage loan = loans[_loanId];
+
+        uint interestAmount = (loan.loanAmount * loan.interestRate) / 100;
+        uint totalRepayment = loan.loanAmount + interestAmount;
+        
+        require(msg.value > totalRepayment, "Insufficient amount to repay loan");
+
+        loan.isRepaid = true;
+
+        payable(loan.lender).transfer(totalRepayment);
+        
+        // Return collateral to borrower
+        payable(loan.borrower).transfer(loan.collateralAmount);
+
+        emit LoanRepaid(_loanId);
+    }
+    // Function to claim collateral on default
+    // Hint: Write the claimCollateral function with necessary checks and logic
+    function claimCollateral(uint _loanId) public payable 
+        validLoan(_loanId)
+        loanFunded(_loanId)
+        notRepaid(_loanId)
+        onlyLender(_loanId) {
+        Loan storage loan = loans[_loanId];
+        require(block.timestamp > loan.dueDate, "Loan is not overdue yet");
+
+        loan.isRepaid = true;
+        payable(loan.lender).transfer(loan.collateralAmount);
+
+        emit CollateralClaimed(_loanId, msg.sender);
+    }
+    
+    function getLoanCount() public view returns (uint) {
+        //nextLoanId starts with 1
+        return nextLoanId - 1; 
+    }
+}
---
+++ test/CollateralizedLoan.js
@@ -0,0 +1,141 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("CollateralizedLoan", function () {
+
+  async function deployCollateralizedLoanFixture() {
+    const [owner, borrower, lender] = await ethers.getSigners();
+
+    const CollateralizedLoan = await ethers.getContractFactory("CollateralizedLoan");
+    const collateralizedLoan = await CollateralizedLoan.deploy();
+
+    return { collateralizedLoan, owner, borrower, lender };
+  }
+
+  describe("Loan Request", function () {
+    it("Should let a borrower deposit collateral and request a loan", async function () {
+      const { collateralizedLoan, borrower } = await loadFixture(deployCollateralizedLoanFixture);
+
+      const collateralAmount = ethers.parseEther("1.0");
+      const interestRate = 16;//my country interest rate
+      const duration = 1;
+
+      await collateralizedLoan
+      .connect(borrower)
+      .depositCollateralAndRequestLoan(interestRate, duration, {
+        value: collateralAmount
+      });
+      //first loan
+      const loanId = 1;
+      const loan = await collateralizedLoan.loans(loanId);
+      expect(loan.borrower).to.equal(borrower.address);
+      expect(loan.collateralAmount.toString()).to.equal(collateralAmount.toString());
+      expect(loan.interestRate).to.equal(interestRate);
+      expect(loan.isFunded).to.equal(false);
+      expect(loan.isRepaid).to.equal(false);
+    });
+  });
+
+  describe("Funding a Loan", function () {
+    it("Allows a lender to fund a requested loan", async function () {
+      // TODO: implement test
+      const { collateralizedLoan, borrower, lender } = await loadFixture(deployCollateralizedLoanFixture);
+
+      const collateralAmount = ethers.parseEther("1.0");
+      const interestRate = 16;
+      const duration = 1;
+
+      await collateralizedLoan
+        .connect(borrower)
+        .depositCollateralAndRequestLoan(interestRate, duration, {
+          value: collateralAmount,
+        });
+      
+      const loanId = 1;
+      const loanData = await collateralizedLoan.loans(loanId);
+
+      await collateralizedLoan
+      .connect(lender)
+      .fundLoad(loanId, 
+        { 
+          value: loanData.loanAmount 
+        }
+      )
+
+      const updated = await collateralizedLoan.loans(loanId);
+      expect(updated.isFunded).to.equal(true);
+      expect(updated.lender).to.equal(lender.address);
+    });
+  });
+
+  describe("Repaying a Loan", function () {
+    it("Enables the borrower to repay the loan fully", async function () {
+      // TODO: implement test
+      const { collateralizedLoan, borrower, lender } = await loadFixture(deployCollateralizedLoanFixture);
+
+      const collateralAmount = ethers.parseEther("1.0");
+      const interestRate = 16;
+      const duration = 1;
+
+      await collateralizedLoan
+        .connect(borrower)
+        .depositCollateralAndRequestLoan(interestRate, duration, {
+          value: collateralAmount,
+        });
+
+      const loanId = 1;
+      const loanData = await collateralizedLoan.loans(loanId);
+      const interest = (loanData.loanAmount * loanData.interestRate) / 100n;
+      const totalRepayment = loanData.loanAmount + interest;
+ 
+      await collateralizedLoan
+      .connect(borrower)
+      .repayLoan(loanId, {
+        value: totalRepayment + 1n
+      })
+
+      const updated = await collateralizedLoan.loans(loanId);
+      expect(updated.isRepaid).to.equal(true);
+    });
+  });
+
+  describe("Claiming Collateral", function () {
+    it("Permits the lender to claim collateral if the loan isn't repaid on time", async function () {
+      // TODO: implement test
+      const { collateralizedLoan, borrower, lender  } = await loadFixture(deployCollateralizedLoanFixture);
+
+      const collateralAmount = ethers.parseEther("1.0");
+      const interestRate = 16;
+      const duration = 1;
+
+      await collateralizedLoan
+        .connect(borrower)
+        .depositCollateralAndRequestLoan(interestRate, duration, {
+          value: collateralAmount,
+        });
+
+      const loanId = 1;
+      const loanData = await collateralizedLoan.loans(loanId);
+
+      //lender funds
+      await collateralizedLoan
+      .connect(lender)
+      .fundLoad(loanId, {
+        value: loanData.loanAmount,
+      });
+
+      //time passed
+      await ethers.provider.send("evm_increaseTime", [2000]);
+      await ethers.provider.send("evm_mine");
+
+
+      await collateralizedLoan
+      .connect(lender)
+      .claimCollateral(loanId)
+
+      const updated = await collateralizedLoan.loans(loanId);
+      expect(updated.isRepaid).to.equal(true);
+    });
+  });
+});
